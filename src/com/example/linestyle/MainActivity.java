dc95febee41893851295e758e5dc2a63d190551d
李林 <ww1095@163.com>
2016-05-27 11:39:16 +0800

    init

---
+++ src/com/example/linestyle/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.linestyle;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+}
